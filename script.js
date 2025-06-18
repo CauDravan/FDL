81b641e648b766660958cd41048f31759f85fb07
CauDravan <99968375+CauDravan@users.noreply.github.com>
2025-06-18 22:44:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const api_key = 'AKfycbzhTUYLpbsJoBnaeXn9uXJx1Ozo8p8oJR2-TNKN0Mnt6s4XgArmZ9AJM_0
 const sheet_id = '1j6RlyzBKN0WX_HsLL4J0mzzF1TzYauxok55dIKA1U2o';
 const range = "'Level(Cd)'!$L:$L";
 
-const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`;
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheet_id}/values/${range}?key=${api_key}`;
 
 fetch(url)
   .then(response => response.json())
